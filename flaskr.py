89f84d41f9fce2bc6b98a3bd78019652ab52e6ff
finn <fterdal@gmail.com>
2015-07-09 16:48:27 -0700

    Initial commit - Step 5 of Flask Tutorial

---
+++ flaskr.py
@@ -0,0 +1,69 @@
+'''
+	Main Application Code
+'''
+
+# Imports
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+from contextlib import closing
+
+# Configuration
+DATABASE = '/tmp/flaskr.db'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'password'
+
+# Create application
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+# Custom configuration overrides from_object(__NAME__) if desired.
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+# Connect the database
+def connect_db():
+	return sqlite3.connect(app.config['DATABASE'])
+
+# Initialize the database
+def init_db():
+	with closing(connect_db()) as db:
+		with app.open_resource('schema.sql', mode='r') as f:
+			db.cursor().executescript(f.read())
+		db.commit()
+
+# Decorators for establishing database connections:
+@app.before_request
+def before_request():
+	g.db = connect_db()
+
+@app.teardown_request
+def teardown_request(exception):
+	db = getattr(g,'db',None)
+	if db is not None:
+		db.close()
+
+# Listens on root to select title and text from the database
+@app.route('/')
+def show_entries():
+	cur = g.db.execute('select title, text from entries order by id desc')
+	entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
+	return render_template('show_entries.html', entries=entries)
+
+# Adds new entries (but only if they are logged in)
+@app.route('/add', methods=['POST'])
+def add_entry():
+	if not session.get('logged_in'):
+		abort(401)
+	g.db.execute('insert into entries (title, text) values (?,?)'),
+			[request.form['title'], request.form['text']])
+	g.db.commit()
+	flash('New entry was successfully posted')
+	return redirect(url_for('show_entries'))
+
+# If this is run directly, start running the app.
+if __name__ == '__main__':
+	app.run()
+
+
+
