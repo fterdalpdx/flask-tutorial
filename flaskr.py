ffd6792b1fbc7f2949f3421110693f9a295f05e0
finn <fterdal@gmail.com>
2015-07-09 17:01:10 -0700

    Added view functions for login/logout - UNTESTED

--- flaskr.py
+++ flaskr.py
@@ -61,9 +61,28 @@ def add_entry():
 	flash('New entry was successfully posted')
 	return redirect(url_for('show_entries'))
 
+# Login, check password, return error if invalid
+def login():
+	error = None
+	if request.method == 'POST':
+		if request.form['username'] != app.config['USERNAME']:
+			error = 'Invalid username'
+		elif request.form['password'] != app.config['PASSWORD']:
+			error = 'Invalid password'
+		else:
+			session['logged_in'] = True
+			flash('You were logged in')
+			return redirect(url_for('show_entries'))
+	return render_template('login.html', error=error)
+
+# Logout
+def logout():
+	session.pop('logged_in', None)
+	flash('You were logged out')
+	return redirect(url_for('show_entries'))
+
 # If this is run directly, start running the app.
 if __name__ == '__main__':
 	app.run()
-
 
 
